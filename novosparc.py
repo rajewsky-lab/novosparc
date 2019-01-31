9d433b46cfc38b831deb335d0bd1dfac75f25ee0
mornitzan <38704420+mornitzan@users.noreply.github.com>
2019-01-30 23:17:42 -0500

    Update novosparc.py

--- novosparc.py
+++ novosparc.py
@@ -145,7 +145,28 @@ def setup_for_OT_reconstruction(dge, locations, num_neighbors=5):
     print ('done (', round(time.time()-start_time, 2), 'seconds )')
     return cost_expression, cost_locations
 
+def setup_for_OT_reconstruction_1d(dge, locations, num_neighbors_source = 5, num_neighbors_target = 2):
+    start_time = time.time()
+    print ('Setting up for reconstruction ... ', end='', flush=True)
 
+    # Shortest paths matrices at target and source spaces
+    num_neighbors_target = num_neighbors_target # number of neighbors for nearest neighbors graph at target
+    A_locations = kneighbors_graph(locations, num_neighbors_target, mode='connectivity', include_self=True)
+    G_locations = nx.from_scipy_sparse_matrix(A_locations)
+    sp_locations = nx.floyd_warshall_numpy(G_locations)
+    num_neighbors_source = num_neighbors_source # number of neighbors for nearest neighbors graph at source
+    A_expression = kneighbors_graph(dge, num_neighbors_source, mode='connectivity', include_self=True)
+    G_expression = nx.from_scipy_sparse_matrix(A_expression)
+    sp_expression = nx.floyd_warshall_numpy(G_expression)
+
+    # Set normalized cost matrices based on shortest paths matrices at target and source spaces
+    cost_locations = sp_locations / sp_locations.max()
+    cost_locations -= np.mean(cost_locations)
+    cost_expression = sp_expression / sp_expression.max()
+    cost_expression -= - np.mean(cost_expression)
+
+    print ('done (', round(time.time()-start_time, 2), 'seconds )')
+    return cost_expression, cost_locations
 
 def find_spatial_archetypes(num_clusters, sdge):
     """Clusters the expression data and finds gene archetypes. Current
