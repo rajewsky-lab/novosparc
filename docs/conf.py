a8a45abf89697cc47788981359457fcba5c4006f
nukappa <nukappa@users.noreply.github.com>
2019-02-21 18:36:34 +0100

    capitalized pot

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
         return MagicMock()
 
-MOCK_MODULES = ['pot']
+MOCK_MODULES = ['POT']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- Project information -----------------------------------------------------
