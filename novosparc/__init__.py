b21eda436d7595941b3cec274d1d9f32a5a718a4
Enes Senel <enessenel@outlook.com>
2021-04-03 14:21:46 +0200

    Updated docs and author info

--- novosparc/__init__.py
+++ novosparc/__init__.py
@@ -1,14 +1,16 @@
 __author__ = ', '.join([
     'Nikos Karaiskos',
     'Mor Nitzan',
-    'Enes Senel'
+    'Enes Senel',
+    'Noa Moriel'
 ])
 __email__ = ', '.join([
     'nikolaos.karaiskos@mdc-berlin.de',
     'mornitzan@fas.harvard.edu',
-    'enes.senel@mdc-berlin.de'
+    'enes.senel@mdc-berlin.de',
+    'noa.moriel@mail.huji.ac.il'
 ])
-__version__ = '0.4.1'
+__version__ = '0.4.2'
 
 from . import plotting as pl
 from . import preprocessing as pp
