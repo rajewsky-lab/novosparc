f7a3302b8c25396260b66dadd675236ccbee6277
nukappa <nukappa@users.noreply.github.com>
2023-11-17 09:48:27 +0100

    fixing rtd

--- docs/conf.py
+++ docs/conf.py
@@ -107,8 +107,8 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-def setup(app):
-    app.add_stylesheet('css/custom.css')
+# def setup(app):
+#     app.add_stylesheet('css/custom.css')
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
